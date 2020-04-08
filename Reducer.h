7a329d05c40676da10a94ac44a250c9aeed27e26
DingZhenyuan <ding_zy@outlook.com>
2020-04-08 23:11:37 +0800

    加入Reducer

---
+++ Reducer.h
@@ -0,0 +1,18 @@
+#ifndef REDUCER_H
+#define REDUCER_H
+
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class Reducer {
+public:
+    Reducer(string fname);
+    virtual ~Reducer(void);
+
+};
+
+#endif // REDUCER_H
