b2751e645e48392278660ce389c54ff1f510831d
DingZhenyuan <ding_zy@outlook.com>
2020-04-08 23:11:11 +0800

    加入Options设定类CGOptions

---
+++ CGOptions.cpp
@@ -0,0 +1,14 @@
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "CGOptions.h"
+#include <iostream>
+#include <cassert>
+#include <cstring>
+#include <map>
+
+
+using namespace std;
+
+
