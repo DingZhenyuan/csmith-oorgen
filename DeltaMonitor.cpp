1cf5bf8c5f55dca5f9a15520222c892a786cdf1c
DingZhenyuan <ding_zy@outlook.com>
2020-04-20 23:39:28 +0800

    管理序列，管理输入输出信息

---
+++ DeltaMonitor.cpp
@@ -0,0 +1,143 @@
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifdef WIN32
+#pragma warning(disable : 4786)   /* Disable annoying warning messages */
+#endif
+
+#include <fstream>
+#include <cassert>
+
+#include "DeltaMonitor.h"
+#include "SequenceFactory.h"
+#include "SimpleDeltaSequence.h"
+#include "SimpleDeltaRndNumGenerator.h"
+#include "random.h"
+#include "RandomNumber.h"
+
+using namespace std;
+
+// dSimpleDelta
+DELTA_TYPE DeltaMonitor::delta_type_ = MAX_DELTA_TYPE;
+
+std::string DeltaMonitor::output_file_ = "";
+std::string DeltaMonitor::input_file_ = "";
+
+bool DeltaMonitor::is_running_ = false;
+bool DeltaMonitor::is_delta_ = false;
+
+bool DeltaMonitor::no_delta_reduction_ = false;
+
+Sequence *DeltaMonitor::seq_ = NULL;
+
+DeltaMonitor::DeltaMonitor() {}
+DeltaMonitor::~DeltaMonitor() {}
+
+Sequence* DeltaMonitor::GetSequence() {
+    assert(delta_type_ != MAX_DELTA_TYPE);
+
+    switch (DeltaMonitor::delta_type_) {
+    case dSimpleDelta:
+        DeltaMonitor::seq_ = SimpleDeltaSequence::CreateInstance(SimpleDeltaSequence::default_sep_char);
+        break;
+    default:
+        assert("DeltaMonitor GetSequence error" && 0);
+        break;
+        
+    }
+    assert(DeltaMonitor::seq_);
+    return DeltaMonitor::seq_;
+}
+
+bool DeltaMonitor::set_delta_type(std::string &msg, const std::string &monitor_type) {
+	if (!monitor_type.compare("simple")) {
+		DeltaMonitor::delta_type_ = dSimpleDelta;
+	} else {
+		msg = "not supported monitor type!";
+		return false;
+	}
+	DeltaMonitor::is_running_ = true;
+	return true;
+}
+
+bool DeltaMonitor::init(std::string &msg, const std::string &monitor_type, const std::string &o_file) {
+	assert(!monitor_type.empty());
+
+	if (o_file.empty()) {
+		msg = "please specify the file for delta output by --delta-output [file]";
+		return false;
+	}
+	DeltaMonitor::output_file_ = o_file;
+
+	if (!DeltaMonitor::set_delta_type(msg, monitor_type)) {
+		return false;
+	}
+
+	return true;
+}
+
+bool DeltaMonitor::init_for_running(std::string &msg, const std::string &monitor_type,
+		const std::string &o_file, const std::string &i_file, bool no_delta) {
+	
+    assert(!monitor_type.empty());
+
+	if (i_file.empty()) {
+		msg = "please specify the file for delta input by --delta-input [file]";
+		return false;
+	}
+    // 若没设定输出文件，则把输入文件路径作为输出文件路径
+	if (o_file.empty()) {
+		DeltaMonitor::output_file_ = i_file;
+	} else {
+		DeltaMonitor::output_file_ = o_file;
+	}
+	DeltaMonitor::input_file_ = i_file;
+    
+    // 设定delta类型
+	if (!DeltaMonitor::set_delta_type(msg, monitor_type)) {
+		return false;
+	}
+
+	DeltaMonitor::is_delta_ = true;
+	DeltaMonitor::no_delta_reduction_ = no_delta;
+	return true;
+}
+
+void DeltaMonitor::OutputStatistics(ostream &out) {
+	switch (DeltaMonitor::delta_type_) {
+	case dSimpleDelta:
+		SimpleDeltaRndNumGenerator::OutputStatistics(out);
+		break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+void DeltaMonitor::Output(ostream &out) {
+	if (!DeltaMonitor::is_running_) return;
+
+	if (is_delta_) DeltaMonitor::OutputStatistics(out);
+
+	assert(!output_file_.empty());
+	std::string s;
+
+	get_sequence(s);
+	ofstream ofile(output_file_.c_str());
+	ofile << s;
+}
+
+
+char DeltaMonitor::GetSepChar() {
+    return SimpleDeltaSequence::default_sep_char;
+}
+
+const std::string& DeltaMonitor::get_input() {
+    return DeltaMonitor::input_file_;
+}
+
+const std::string& DeltaMonitor::get_output() {
+    return DeltaMonitor::output_file_;
+}
+
